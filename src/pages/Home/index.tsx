4e1fb2c8e053119b1ca3fcfe502e1d9906a41070
Deustavo <gustavo.andrade00@outlook.com>
2021-03-24 04:18:58 -0300

    minha versão do chuck norris jokes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
-
-import LoadGif from '../../img/giphy.gif';
+import LogoGif from '../../img/giphy.gif';
+import LoadingChuck from '../../img/head-chuck.png';
 import { HomeContainer } from './style';
 import api from '../../service/api';
+
 
 interface IJoke{
     id: string;
@@ -12,75 +13,120 @@ interface IJoke{
 
 const Home: React.FC = () => {
     const [ categoriesJoke, setCategoriesJoke ] = useState([]);
-    const [ categorySelected, setCategorySelected ] = useState<IJoke>()
-    const [ searchJoke, setSearchJoke ] = useState('')
-    const [ isLoad, setIsLoad ] = useState(false)
-    const [ resultSearch, setResultSearch ] = useState<IJoke[]>([])
+    const [ allJokes, setAllJokes] = useState(false);
+    const [ isLoad, setIsLoad ] = useState(false);
+    const [ resultSearch, setResultSearch ] = useState<IJoke[]>([]);
+    // const [ searchJoke, setSearchJoke ] = useState('');
+    // const [ categorySelected, setCategorySelected ] = useState<IJoke>();
 
 
     useEffect( () => {
         api.get('jokes/categories').then(
             response => {
-                setCategoriesJoke(response.data)
+                setCategoriesJoke(response.data);
             }
         )
     }, [])
 
-    async function handleJokes(){
-        setIsLoad(true)
-        const response = await api.get(`jokes/search?query=${searchJoke}`)
-        setResultSearch(response.data.result)
-        if(response.status === 200 ){
-            setIsLoad(false)
+    async function handleJokes(e: string){
+        setIsLoad(true);
+        try {
+            if(allJokes) {
+                var response = await api.get(`jokes/search?query=${e}`);
+                setResultSearch(response.data.result);
+            } else {
+                var response = await api.get(`jokes/random?category=${e}`);
+                setResultSearch([response.data]);
+            }
+
+            if(response.status === 200 ){
+                setIsLoad(false);
+            }
+        } catch {
+            setIsLoad(false);
+            setResultSearch([]);
         } 
     }
 
-    async function handleJokeByCategory(e: string){
-        setIsLoad(true)
-        const response = await api.get(`jokes/random?category=${e}`)
-        setCategorySelected(response.data)
-        if( response.status === 200 ){ 
-            setIsLoad(false)
-        } 
-
-
-
-    }
+    // async function handleJokeByCategory(e: string){
+    //     setIsLoad(true);
+    //     const response = await api.get(`jokes/random?category=${e}`);
+    //     setCategorySelected(response.data);
+    //     if( response.status === 200 ){ 
+    //         setIsLoad(false);
+    //     } 
+    // }
 
 
     return (
         <HomeContainer>
-            <div className="categories">
-                <ul>
-                { categoriesJoke.map( (joke, index) => (
-                    <li key={index} onClick={ () => handleJokeByCategory(joke) }>{joke}</li>
-                ))}
-                </ul>
-            </div>
-            <div>
-                <img src={categorySelected?.icon_url} alt={categorySelected?.value}/>
-                <h3>{categorySelected?.value}</h3>
-            </div>
-
-            <h2>Find Joke</h2>
+            <img className="logo-page" src={LogoGif} alt="chuck"/>
+            <h1 className="title-page">Chuck Norris Jokes</h1>
 
             <div className="input-group">
-                <input type="text" onChange={ e => setSearchJoke(e.target.value)} />
-                <button type="submit" onClick={handleJokes}> Find joke</button>
+                <h2>Choose a joke category</h2>
+                <div>
+                    <select className="input-main" defaultValue={0} onChange={(e) => handleJokes(e.target.value)}>
+                        <option disabled selected value={0}> -- select an option -- </option>
+                        {categoriesJoke.map( (joke, index) => (
+                            <option key={index} value={joke}>{joke}</option>
+                        ))}
+                    </select>
+                </div>
+                <div className="input-options">
+                    <div onClick={() => setAllJokes(false)}>
+                        <input type="radio" id="random" name="type-joke" value="random" defaultChecked/>
+                        <label htmlFor="random">A random joke</label>
+                    </div>
+                    <div onClick={() => setAllJokes(true)}>
+                        <input type="radio" id="all" name="type-joke" value="all" />
+                        <label htmlFor="all">All jokes in category</label>
+                    </div>
+                </div>
             </div>
-            <p>Find results for:</p>
-            <div className="joker">
-                { isLoad ? <img src={LoadGif} alt="load"/> : resultSearch.map( result => (
-                    <div key={result.id}>
+            
+            <div className="joker-group">
+                { isLoad ? <img className="loading-image rotating" src={LoadingChuck} alt="load"/> : resultSearch.map( result => (
+                    <div className="joker" key={result.id}>
                         <img src={result.icon_url} alt={result.value}/>
                         <h3>{result.value}</h3>
                     </div>
                 )) }
-
             </div>
-            
-            
 
+            {/* <div className="categories">
+                <h2>Random Joke</h2>
+                <div>
+                    <select>
+                        <option>Selecione uma categoria</option>
+                        {categoriesJoke.map( (joke, index) => (
+                            <option key={index} value={joke} onClick={ () => handleJokeByCategory(joke) }>{joke}</option>
+                        ))}
+                    </select>
+                </div>
+                <div>
+                    <img src={categorySelected?.icon_url} alt={categorySelected?.value}/>
+                    <h3>{categorySelected?.value}</h3>
+                </div>
+            </div> */}
+
+            {/* <div>
+                <h2>Find Joke</h2>
+                <div className="input-group">
+                    <input type="text" onChange={ e => setSearchJoke(e.target.value)} />
+                    <button type="submit" onClick={handleJokes}> Find joke</button>
+                </div>
+                <p>Find results for:</p>
+                <div className="joker">
+                    { isLoad ? <img src={LogoGif} alt="load"/> : resultSearch.map( result => (
+                        <div key={result.id}>
+                            <img src={result.icon_url} alt={result.value}/>
+                            <h3>{result.value}</h3>
+                        </div>
+                    )) }
+
+                </div>
+            </div> */}
         </HomeContainer>
     );
 }
--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -3,17 +3,114 @@ import styled from 'styled-components';
 export const HomeContainer = styled.div`
     width: 80%;
     margin: auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
-    .categories{
+    .logo-page {
+        width: 20%;
+        margin-top: 10%;
+    }
+
+    .input-options {
         display: flex;
+        justify-content: space-around;
+        font-size: 20px;
+        margin: 12px 0;
     }
 
-    .input-group{
-
+    .input-group {
+        width: calc(64% - 24px);
+        margin-top: 5%
     }
 
-    .joker{
-        display: block;
+    .title-page {
+        font-family: 'Lobster', cursive;
+        font-size: 4vw;
+        margin-top: -3vw;
+        
+        -webkit-text-fill-color: #000; 
+        -webkit-text-stroke-width: .05px;
+        -webkit-text-stroke-color: #fff;
     }
 
+    .input-main {
+        width: 100%;
+        padding: 12px;
+        margin: 12px 0;
+        border-radius: 6px;
+    }
+
+    .joker-group {
+        width: calc(64% - 24px);
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .loading-image {
+        width: 40%
+    }
+
+    .joker {
+        display: flex;
+        padding: 0 0 40px 0;
+        margin: 40px 0 0 0;
+        border-bottom: solid 1px rgba(0, 0, 0, 0.1);
+        width: 100%
+    }
+
+    .joker img {
+        height: 60px;
+        margin: -8px 24px 0 0;
+    }
+
+    @media only screen and (max-width: 1024px) {
+        .title-page {
+            font-size: 7vw;
+            margin-top: -5vw;
+        }
+        .logo-page {
+            width: 40%;
+        }
+    }
+
+
+
+    
+    @-webkit-keyframes rotating /* Safari and Chrome */ {
+        from {
+          -webkit-transform: rotate(0deg);
+          -o-transform: rotate(0deg);
+          transform: rotate(0deg);
+        }
+        to {
+          -webkit-transform: rotate(360deg);
+          -o-transform: rotate(360deg);
+          transform: rotate(360deg);
+        }
+      }
+      @keyframes rotating {
+        from {
+          -ms-transform: rotate(0deg);
+          -moz-transform: rotate(0deg);
+          -webkit-transform: rotate(0deg);
+          -o-transform: rotate(0deg);
+          transform: rotate(0deg);
+        }
+        to {
+          -ms-transform: rotate(360deg);
+          -moz-transform: rotate(360deg);
+          -webkit-transform: rotate(360deg);
+          -o-transform: rotate(360deg);
+          transform: rotate(360deg);
+        }
+      }
+      .rotating {
+        -webkit-animation: rotating 1s linear infinite;
+        -moz-animation: rotating 1s linear infinite;
+        -ms-animation: rotating 1s linear infinite;
+        -o-animation: rotating 1s linear infinite;
+        animation: rotating 1s linear infinite;
+      }
 `
